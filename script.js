5fa2ff4518968667635ad09ef39e66b0b89b835e
Rain Raquipiso <87350585+rainraqui@users.noreply.github.com>
2023-11-04 20:50:46 +1300

    Fixed nav bar

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function toggleMenu(){
+    console.log("toggleMenu() called");
     const menu = document.querySelector(".menu-links")
     const icon = document.querySelector(".hamburger-icon")
     menu.classList.toggle("open")
     icon.classList.toggle("open")
+}
+
+function scrollToTop() {
+    console.log("scrollToTop() called");
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+
+    history.replaceState({}, document.title, window.location.pathname);
+}  
+
+window.onscroll = function() {fixedHeader()};
+var header = document.getElementById("desktop-nav");
+var sticky = header.offsetTop;
+function fixedHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
