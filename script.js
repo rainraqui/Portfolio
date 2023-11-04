1766602c6c41af45ce4eb0231ae36bff18319ca2
Rain Raquipiso <87350585+rainraqui@users.noreply.github.com>
2023-11-04 22:24:16 +1300

    removed assets, bug fix on hamburger-menu links

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-function toggleMenu(){
-    console.log("toggleMenu() called");
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+function toggleMenu() {
+  console.log("toggleMenu() called");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
 function scrollToTop() {
-    console.log("scrollToTop() called");
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+  console.log("scrollToTop() called");
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 
-    history.replaceState({}, document.title, window.location.pathname);
-}  
+  history.replaceState({}, document.title, window.location.pathname);
+}
 
-window.onscroll = function() {fixedHeader()};
+function scrollToSection(sectionId) {
+  const section = document.querySelector(sectionId);
+  if (section) section.scrollIntoView({ behavior: "smooth" });
+  history.pushState({}, "", sectionId);
+  toggleMenu();
+}
+
+window.onscroll = function () {
+  fixedHeader();
+};
 var header = document.getElementById("desktop-nav");
+var mobileHeader = document.getElementById("hamburger-nav");
 var sticky = header.offsetTop;
+var mobileSticky = mobileHeader.offsetTop;
 function fixedHeader() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
+  }
+
+  if (window.pageYOffset > mobileSticky) {
+    mobileHeader.classList.add("sticky");
+  } else {
+    mobileHeader.classList.remove("sticky");
   }
 }
